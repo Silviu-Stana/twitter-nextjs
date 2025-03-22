4f827dce7e8d5aa7cf7cead8179db980c226e29b
Seal <gamedevseals@gmail.com>
2025-03-22 09:57:48 +0200

    Login Modal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import Layout from '@/components/Layout';
-import Modal from '@/components/Modal';
+import LoginModal from '@/components/modals/LoginModal';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Modal actionLabel="Submit" title="Test Modal" isOpen />
+            <LoginModal />
             <Layout>
                 <Component {...pageProps} />
             </Layout>
